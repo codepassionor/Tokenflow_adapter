0f347ef5c92e75c169c873215b6ee1575b585e0e
JackHe <116295741+codepassionor@users.noreply.github.com>
2024-03-23 02:20:21 -0500

    Update main.py

--- vid_edit_friendly_t2i/main.py
+++ vid_edit_friendly_t2i/main.py
@@ -776,7 +776,9 @@ def main():
                     loss = F.mse_loss(model_pred.float(), target.float(), reduction="none")
                     loss = loss.mean(dim=list(range(1, len(loss.shape)))) * mse_loss_weights
                     loss = loss.mean()
-                features = hook.get_features()
+                # features = hook.get_features()
+                features = [torch.randn((4, 32, 64, 64)), torch.randn((4, 64, 32, 32))]
+                cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
                 for feature in features : 
                    f1_t1 = feature[0::4] # b//4 c h w
                    f2_t1 = feature[1::4] 
@@ -789,13 +791,11 @@ def main():
                    tmp4 = torch.permute(f2_t2, (0, 2, 3, 1)).reshape(-1, c)
                    sim_12 = cos(tmp1, tmp2) 
                    sim_34 = cos(tmp3, tmp4)
-                   loss += ((sim_12 - sim_34) ** 2).mean()
-                else:
-                    our_loss = 0
-                    print('our_loss = ', our_loss)
-                loss += our_loss
+                   our_loss = ((sim_12 - sim_34) ** 2).mean()
+                   print('our_loss = ', our_loss)
+                   loss += our_loss
                 print('loss = ', loss)
-                hook.reset()
+                # hook.reset()
 
                 # Gather the losses across all processes for logging (if we use distributed training).
                 avg_loss = accelerator.gather(loss.repeat(args.train_batch_size)).mean()
