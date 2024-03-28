4ffa4839f6fec39cc7d6849cb7c19ccff4caa4ff
ShuYuMo2003 <244679245@qq.com>
2024-03-28 18:54:53 +0800

    feat(script): add example of usage of webdataset.

---
+++ script/msrvtt-webdataset-usage-test.py
@@ -0,0 +1,18 @@
+import torch
+import torchvision.transforms as transforms
+import webdataset as wds
+from io import BytesIO
+from PIL import Image
+
+dataset = wds.WebDataset("/root/autodl-tmp/msrvtt.tar").decode(
+    wds.handle_extension("input.ppm", transforms.Compose([
+            lambda x : Image.open(BytesIO(x)),
+            transforms.ToTensor()
+        ])),
+)
+
+for i, sample in enumerate(dataset):
+    for key, value in sample.items():
+        print(key, type(value), repr(value)[:100])
+    print()
+    if i >= 5: break
