11cba33ea400c6d380b18d45e1e1d6c5b28e88b3
DuHeng0128 <132717938+DuHeng0128@users.noreply.github.com>
2024-08-28 12:56:00 +0800

    Add files via upload

---
+++ vidtome/run.sh
@@ -0,0 +1,2 @@
+export XDG_CACHE_HOME=/root/autodl-tmp/cache_huggingface/; 
+export HF_ENDPOINT=https://hf-mirror.com; python main.py
---
+++ vidtome/run_timesteps.sh
@@ -0,0 +1,13 @@
+export XDG_CACHE_HOME=/root/autodl-tmp/cache_huggingface/; 
+export HF_ENDPOINT=https://hf-mirror.com;
+# 运行 main.py 并传递第一个参数对
+python main.py --begin=0 --end=300
+
+# 运行 main.py 并传递第二个参数对
+python main.py --begin=300 --end=600
+
+# 运行 main.py 并传递第三个参数对
+python main.py --begin=600 --end=900
+
+# 运行 main.py 并传递第三个参数对
+python main.py --begin=900 --end=1000
